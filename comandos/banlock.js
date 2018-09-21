693b22ef5ac03edfe1587c2dc812e651f59bd73d
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-09-21 12:19:34 -0300

    Update banlock.js

--- comandos/banlock.js
+++ comandos/banlock.js
@@ -14,6 +14,14 @@ db.Bloqueio.findOne({
         
 }
 
+   const ID = '307956208042770433';	
+  let users = client.guilds.get(ID).members.get(message.author.id);
+  if (!users) 
+  return message.reply("Você não está no servidor oficial do Sysop,pega o convite usando **sy!infos**");
+  let role = client.guilds.get(ID).members.get(message.author.id).roles.find("id", "439588232263499776")
+  if(!role)
+  return message.channel.send(`<:DiscordPartner:467546781538975754> Woww ${message.author}! Você não é um usuário Parceiro do Sysop. Mais infos de como virar parceiro digite: sy!partner.`);
+  
 
   
             let args = suffix.split(' '); 
